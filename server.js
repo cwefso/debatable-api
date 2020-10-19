b8888bca09076fe56c4fec48fc6079c16176827d
Charles Wefso <cwefso@gmail.com>
2020-10-19 12:33:44 -0600

    restructures endpoints for players array

--- server.js
+++ server.js
@@ -1,84 +1,123 @@
-import cors from 'cors';
-import request from 'request'
+import cors from "cors";
+import request from "request";
 import fetch from "node-fetch";
-import express from 'express';
-var app     = express()
-import shuffle from 'shuffle-array';
+import express from "express";
+var app = express();
+import shuffle from "shuffle-array";
 import { characters } from "./topics/characters.js";
-import { creatures } from "./topics/creatures.js"
-import { science } from './topics/science.js';
-import { movies } from './topics/movies.js';
-import { tv } from './topics/tv.js';
-import { historical } from './topics/historical.js';
-import { disaster } from './topics/disaster.js';
-import { celebrity } from './topics/celebrity.js';
-import { politics } from './topics/politics.js';
-import { places } from './topics/places.js';
+import { creatures } from "./topics/creatures.js";
+import { science } from "./topics/science.js";
+import { movies } from "./topics/movies.js";
+import { tv } from "./topics/tv.js";
+import { historical } from "./topics/historical.js";
+import { disaster } from "./topics/disaster.js";
+import { celebrity } from "./topics/celebrity.js";
+import { politics } from "./topics/politics.js";
+import { places } from "./topics/places.js";
 
-fetch(request, {mode: 'cors'});
+fetch(request, { mode: "cors" });
 
-app.set('port', process.env.PORT || 8000);
+app.use(express.json());
+app.set("port", process.env.PORT || 8000);
 app.use(cors());
-app.use(express.static("build"))
 
-app.locals.title = 'Debatable Backend';
+app.use(express.static("build"));
+
+app.locals.title = "Debatable Backend";
 
 app.locals.topics = [
-  characters[0],
-  creatures[0],
-  science[0],
-  movies[0],
-  tv[0],
-  historical[0],
-  disaster[0],
-  celebrity[0],
-  politics[0],
-  places[0]
-]
+	characters[0],
+	creatures[0],
+	science[0],
+	movies[0],
+	tv[0],
+	historical[0],
+	disaster[0],
+	celebrity[0],
+	politics[0],
+	places[0],
+];
 
-app.get('/', (request, response) => {
-  response.send('Oh hey Debatable');
+app.locals.players = [];
+
+app.get("/", (request, response) => {
+	response.send("Debatable Backend");
 });
 
-app.get('/api/v1/all-topics', (request, response) => {
-  const topics = app.locals.topics
+app.get("/api/v1/all-topics", (request, response) => {
+	const topics = app.locals.topics;
 
-    if(!topics) {
-      return response.sendStatus(404)
-    }
+	if (!topics) {
+		return response.sendStatus(404);
+	}
 
-  response.json({ topics });
+	response.json({ topics });
 });
 
-app.get('/api/v1/topics', (request, response) => {
-  const topic1 = shuffle.pick(app.locals.topics);
-  const topic2 = shuffle.pick(app.locals.topics.filter(topic => topic1.name !== topic.name));
+app.get("/api/v1/topic", (request, response) => {
+	const topic1 = shuffle.pick(app.locals.topics);
+	app.locals.topics = app.locals.topics.filter(
+		(topic) => topic1.name !== topic.name
+  );
+  
+  if(app.locals.players.length === 2){
+    app.locals.players.shift()
+  }
+  
+	app.locals.players.push(topic1);
 
-  const topics = [
-    topic1,
-    topic2
-  ]
+	const topic = topic1;
 
-    if(!topics) {
-      return response.sendStatus(404)
-    }
+	if (!topic) {
+		return response.sendStatus(404);
+	}
 
-  response.json({ topics });
+	response.json({ topic });
 });
 
-app.get('/api/v1/topic', (request, response) => {
-  const topic1 = shuffle.pick(app.locals.topics);
-  app.locals.topics = app.locals.topics.filter(topic => topic1.name !== topic.name);
+app.get("/api/v1/clear", (request, response) => {
+  app.locals.players = []
+  
+  players = app.locals.players
 
-  const topic = topic1
-
-    if(!topic) {
-      return response.sendStatus(404)
-    }
-
-  response.json({ topic });
+	response.json({ players });
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+app.get("/api/v1/players", (request, response) => {
+	const players = app.locals.players;
+
+	if (!players) {
+		return response.sendStatus(404);
+	}
+
+	response.json({ players });
+});
+
+
+app.delete("/api/v1/favorites/:contentId", (request, response) => {
+	const { contentId } = request.params;
+	const parsedId = parseInt(contentId);
+	const match = app.locals.favorites.find(
+		(painting) => parseInt(painting.contentId) === parsedId
+	);
+
+	if (!match) {
+		return response
+			.status(404)
+			.json({ error: `No painting found with an id of ${contentId}.` });
+	}
+
+	const updatedPaintings = app.locals.favorites.filter(
+		(painting) => parseInt(painting.contentId) !== parsedId
+	);
+
+	app.locals.favorites = updatedPaintings;
+
+	return response.status(202).json(app.locals.favorites);
+});
+
+app.listen(app.get("port"), () => {
+	console.log(
+		`${app.locals.title} is running on http://localhost:${app.get("port")}.`
+	);
 });
